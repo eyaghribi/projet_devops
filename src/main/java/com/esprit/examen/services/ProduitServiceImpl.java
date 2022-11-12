fea837b96b3800b497681a7456be7f29a057b0a1
RamseyBerriri <ramzi.berriri@esprit.tn>
2022-11-12 01:40:15 +0100

    Update ProduitServiceImpl.java

--- src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
+++ src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
@@ -1,16 +1,18 @@
 package com.esprit.examen.services;
 
-import java.util.Date;
 import java.util.List;
+
 import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.esprit.examen.entities.CategorieProduit;
+
 import com.esprit.examen.entities.Produit;
 import com.esprit.examen.entities.Stock;
 import com.esprit.examen.repositories.CategorieProduitRepository;
 import com.esprit.examen.repositories.ProduitRepository;
 import com.esprit.examen.repositories.StockRepository;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -26,11 +28,9 @@ public class ProduitServiceImpl implements IProduitService {
 
 	@Override
 	public List<Produit> retrieveAllProduits() {
-		List<Produit> produits = (List<Produit>) produitRepository.findAll();
-		for (Produit produit : produits) {
-			log.info(" Produit : " + produit);
-		}
-		return produits;
+
+		
+		return  produitRepository.findAll();
 	}
 
 	@Transactional
@@ -53,9 +53,7 @@ public class ProduitServiceImpl implements IProduitService {
 
 	@Override
 	public Produit retrieveProduit(Long produitId) {
-		Produit produit = produitRepository.findById(produitId).orElse(null);
-		log.info("produit :" + produit);
-		return produit;
+		return  produitRepository.findById(produitId).orElse(null);
 	}
 
 	@Override
